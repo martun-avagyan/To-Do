edbc2111e9977bee06e085c64c752dee16485a34
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-02-28 16:19:34 +0400

    feart: add plugin to minify css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
+const { appendFile } = require("fs");
 const path = require("path");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/App.js",
   output: {
-    filename: "bundle.js",
+    filename: "App.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "none",
   module: {
     rules: [
       {
-        test: /\.css/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css/, // All css files
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Webpack reads from right to left, "style-loader" - for bundling css with js
+      },
+      {
+        test: /\.js$/, // All js files
+        exclude: /node_modules/, // Except
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"], // can add as many plugins as needed for babel
+          },
+        },
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "App.css",
+    }),
+  ], //
 };
